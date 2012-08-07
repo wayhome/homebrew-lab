52dcf7a0f507e9cf1ee563eb87d5834f73be03fd
Young King <y@zhihu.com>
2012-08-07 21:10:07 +0800

    update vim

--- vim.rb
+++ vim.rb
@@ -1,24 +1,55 @@
 require 'formula'
 
-class Vim <Formula
-  url 'ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2'
+class Vim < Formula
   homepage 'http://www.vim.org/'
-  md5 '5b9510a17074e2b37d8bb38ae09edbf2'
+  url 'ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2'
+  head 'https://vim.googlecode.com/hg/'
+  sha256 '5c5d5d6e07f1bbc49b6fe3906ff8a7e39b049928b68195b38e3e3d347100221d'
+  version '7.3.618'
 
-  def patchlevel; 69 end
   def features; %w(tiny small normal big huge) end
+  def interp; %w(lua mzscheme perl python python3 tcl ruby) end
 
   def options
-    features.map {|f| ["--#{f}", "Configure with --with-feature-#{f}"] }
+    [
+      ["--with-features=TYPE", "tiny, small, normal, big or huge (default: normal)"],
+      ["--enable-interp=NAME,...", "lua, mzscheme, perl, python, python3, tcl and/or ruby"]
+    ]
   end
 
   def patches
-    patches = (1..patchlevel).map {|i| sprintf('ftp://ftp.vim.org/pub/vim/patches/7.3/7.3.%03d', i) }
-    {:p0 => patches}
+    patch_level = version.split('.').last.to_i
+    {'p0' => (1..patch_level).map { |i| 'ftp://ftp.vim.org/pub/vim/patches/7.3/7.3.%03d' % i }}
   end
 
   def install
-    feature = features.find {|f| ARGV.include? "--#{f}" } || "normal"
+    def opt_val(opt)
+      opt.sub(/.*?=(.*)$/, "\\1") rescue nil
+    end
+
+    opts = []
+    feature = opt_val(ARGV.find {|s| s =~ /^--with-features=/ }) || "normal"
+    # For compatibility and convenience {{{
+    feature_shorthand = features.find {|f| ARGV.include? "--#{f}" }
+    feature = feature_shorthand if feature_shorthand
+    # }}}
+    opts << "--with-features=#{feature}"
+
+    interps = opt_val(ARGV.find {|s| s =~ /^--enable-interp=/ }) || ""
+    interps = interps.split(/,/)
+    # For compatibility and convenience {{{
+    interp.each do |i|
+      if ARGV.include? "--#{i}"
+        interps << i
+      end
+    end
+    # }}}
+    interps.uniq!
+    interps.each do |i|
+      opts << "--enable-#{i}interp=yes"
+      opts << "--with-lua-prefix=/usr/local" if i == "lua"
+    end
+
     system "./configure",
       "--disable-gui",
       "--without-x",
@@ -26,10 +57,9 @@ class Vim <Formula
       "--disable-nls",
       "--with-tlib=ncurses",
       "--enable-multibyte",
-      "--enable-pythoninterp",
-      "--with-feature-#{feature}",
-      "--prefix=#{prefix}"
-      "--mandir=#{man}"
+      "--prefix=#{prefix}",
+      "--mandir=#{man}",
+      *opts
     system "make install"
   end
 end
