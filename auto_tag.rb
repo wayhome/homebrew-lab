87fad4325705f553ae7fed610f4d4790af535e98
YoungKing <yanckin@gmail.com>
2013-01-28 14:56:37 +0800

    upgrade auto_tag

--- auto_tag.rb
+++ auto_tag.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class AutoTag < GithubGistFormula
   homepage 'http://zhihu.com'
-  url 'https://gist.github.com/raw/4c0b9d1d75766781dbf6/c620aa740fded04de3f2075dfb941ed6aabd534e/auto_tag'
-  md5 '1e372426170d1365276d288809e4d235'
-  version 'v0.0.1'
+  url 'https://gist.github.com/raw/4c0b9d1d75766781dbf6/003dea6fcb64e721379ac005f0fa12c5565d2fc9/auto_tag'
+  md5 'cd09f236c15d1d37b779c01bc3be01c3'
+  version 'v0.0.2'
 end
