8dae10f7a2f241c610991d493e3da73153d4413e
YoungKing <yanckin@gmail.com>
2013-01-28 14:43:32 +0800

    add auto_tag

---
+++ auto_tag.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class AutoTag < GithubGistFormula
+  homepage 'http://zhihu.com'
+  url 'https://gist.github.com/raw/4c0b9d1d75766781dbf6/c620aa740fded04de3f2075dfb941ed6aabd534e/auto_tag'
+  md5 '1e372426170d1365276d288809e4d235'
+  version 'v0.0.1'
+end
