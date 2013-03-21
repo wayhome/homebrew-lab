176c8a148787c54cd85fd66e95f4b918b65fde44
YoungKing <yanckin@gmail.com>
2013-03-21 22:38:41 +0800

    update wish formula

--- wish.rb
+++ wish.rb
@@ -2,8 +2,6 @@ require 'formula'
 
 class Wish < Formula
   homepage 'http://zhihu.com'
-  url 'http://git.in.zhihu.com/infrastructure/wish.git', :tag => 'v0.2.4'
-  version 'v0.2.4'
   head 'git@git.in.zhihu.com:infrastructure/wish.git', :using => :git
 
   def install
