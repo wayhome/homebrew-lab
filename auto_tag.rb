e2f588cd530b106826bacad1ebfecdefe55d4912
YoungKing <yanckin@gmail.com>
2013-05-02 12:19:03 +0800

    remove private formula

--- auto_tag.rb
+++
@@ -1,8 +0,0 @@
-require 'formula'
-
-class AutoTag < GithubGistFormula
-  homepage 'http://zhihu.com'
-  url 'https://gist.github.com/raw/4c0b9d1d75766781dbf6/003dea6fcb64e721379ac005f0fa12c5565d2fc9/auto_tag'
-  md5 'cd09f236c15d1d37b779c01bc3be01c3'
-  version 'v0.0.2'
-end
